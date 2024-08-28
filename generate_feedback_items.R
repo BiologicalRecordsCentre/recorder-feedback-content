66e90cc195cafebe8ede3f67ecc3c67badb0be3c
Simon Rolph <simrol@ceh.ac.uk>
2024-08-28 09:36:30 +0100

    Filepath no longer hard coded in generate_feedback_items.R

--- generate_feedback_items.R
+++ generate_feedback_items.R
@@ -1,15 +1,16 @@
 # This is a script that can be run from command line (e.g. on a schedule) 
 print(getwd())
-setwd("C:/Users/simrol/Documents/python/recorder-feedback-controller/R/myrecord_weekly")
+setwd("./R/myrecord_weekly")
 print(getwd())
 
 if(!require(renv)){
-  install.packages("renv")
+  install.packages("renv",repos='http://cran.r-project.org')
   library(renv)
 }
 
 #activate the renv environment
 source("renv/activate.R")
+renv::restore()
 
 # run the pipeline
 targets::tar_make()
