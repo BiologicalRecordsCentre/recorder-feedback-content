c757d24f94e0e894a3d06483faf2cd4d7af9f545
Simon Rolph <simrol@ceh.ac.uk>
2025-04-23 14:24:25 +0100

    update run_pipeline.sh to include absolute paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Define base directory (change this to where your script actually lives)
+BASE_DIR="/home/simrol/recorder-feedback-content"
+
+# Navigate to the working directory
+cd "$BASE_DIR" || exit
+
 # Generate datetime string in format YYYY-MM-DD_HH-MM-SS
 datetime_str=$(date +"%Y-%m-%d_%H-%M-%S")
 
