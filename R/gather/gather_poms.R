333fb23087692e5b748df2dd44edf85b3cd670e1
Simon Rolph <simrol@ceh.ac.uk>
2025-06-26 10:12:56 +0100

    Update to gather_poms: get users and save data

--- R/gather/gather_poms.R
+++ R/gather/gather_poms.R
@@ -1,7 +1,11 @@
+print("Loading R packages...")
 library(RJDBC)
+config <- config::get()
 
+print("Loading JDBC driver...")
 drv <- JDBC(driverClass = "org.postgresql.Driver",classPath = Sys.getenv("JAVA_PATH"))
 
+print("Connecting to database...")
 con <- dbConnect(
   drv,
   Sys.getenv("DB_CONNECTION"),
@@ -9,9 +13,18 @@ con <- dbConnect(
   Sys.getenv("DB_PASSWORD")
   )
 
-query <- readLines("sql/poms_query.sql") |> paste(collapse = "\n")
+print("Loading subscribers...")
+subscribers_df <- read.csv(config$participant_data_file)
 
+print("Building query...")
+query <- readLines("sql/poms_query.sql") |> paste(collapse = "\n")
+query <- gsub("FIND_REPLACE_USER_IDS",paste0(subscribers_df$user_id,collapse = ","),query)
+
+print("Querying database...")
 result <- dbGetQuery(con, query)
+result$user_id <- result$digitised_by
+
 print(result)
 
-
+print("Saving data...")
+write.csv(result,config$data_file,row.names = F)#save the data
