097f584a5d03dfa393359f998259cdc199ccd0aa
Simon Rolph <simrol@ceh.ac.uk>
2025-06-27 16:16:16 +0100

    Provide some gather_poms instructions and remove the .jar file i didn't mean to commit

--- R/gather/gather_poms.R
+++ R/gather/gather_poms.R
@@ -1,3 +1,19 @@
+# Implementation notes
+#
+# PoMS data is gathered from the indica warehouse. This uses a JDBC connection to the replica warehouse running on a UKCEH scicom virtual machine
+#
+# You need to create poms.sql in the sql folder, you can get a copy of this from the private repo here (you need to request access from Robin): https://github.com/robin-hutchinson/indicia_reporting/blob/main/survey/ukpoms/fit_counts/sql/2023_data
+#
+# You will need to download a postgresql java driver from here: https://jdbc.postgresql.org/download/ and save it in the java folder, then provide an environment variable in .Renviron e.g. JAVA_PATH="java/your_java_driver.jar"
+#
+# You will also need to set environment variables for the connection (DB_CONNECTION, DB_USERNAME, DB_PASSWORD)
+# The connection will be in a format like jdbc:postgresql://virtualmachine.scicom.ceh.ac.uk:5432/databasename
+#
+# You also need to install R package RJDBC
+#
+# Cross you fingers and hope it works!
+
+
 print("Loading R packages...")
 library(RJDBC)
 config <- config::get()
