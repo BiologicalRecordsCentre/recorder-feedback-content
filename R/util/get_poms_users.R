68fca6a0f5410d559d2875b6159e975b6e4a1c74
Simon Rolph <simrol@ceh.ac.uk>
2025-08-05 11:28:53 +0100

    poms users

---
+++ R/util/get_poms_users.R
@@ -0,0 +1,32 @@
+#see R/gather/gather_poms for more information on the database connection.
+
+print("Loading R packages...")
+library(RJDBC)
+library(dplyr)
+config <- config::get()
+
+print("Loading JDBC driver...")
+drv <- JDBC(driverClass = "org.postgresql.Driver",classPath = Sys.getenv("JAVA_PATH"))
+
+print("Connecting to database...")
+con <- dbConnect(
+  drv,
+  Sys.getenv("DB_CONNECTION"),
+  Sys.getenv("DB_USERNAME"),
+  Sys.getenv("DB_PASSWORD")
+)
+
+
+print("Building query...")
+query <- readLines("sql/poms_users.sql") |> paste(collapse = "\n")
+
+
+
+print("Querying database...")
+result <- dbGetQuery(con, query)
+
+result <- result %>% rename("email"="email_address") %>% mutate(email =tolower(email))
+
+print("Saving data...")
+write.csv(result,"data/poms_partipants.csv" ,row.names = FALSE)
+print("Data saved")
